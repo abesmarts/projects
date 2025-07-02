ccc6a84d5a2d671d26308e7f66e43deae60b3775
abner espinoza <abespinoza155@gmail.com>
2025-07-02 12:00:18 -0400

    adding main back

--- opentofu-semaphore-ansible/opentofu/kubernetes.tf
+++ opentofu-semaphore-ansible/opentofu/kubernetes.tf
@@ -1,17 +1,4 @@
-terraform {
-    required_version = ">= 1.0"
-    required_providers {
-        kubernetes = {
-            source = "hashicorp/kubernetes"
-            version = "~> 2.23.0"
-        }
-    }
-}
 
-provider "kubernetes" {
-    config_path = "/home/semaphore/.kube/config"
-    config_context = "minikube"
-}
 
 resource "kubernetes_namespace" "opentofu_ansible" {
     metadata {
